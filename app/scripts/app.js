e1eee009ab2007276e3f5fd19a1b4e9a278e857f
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-02-19 16:07:29 +0300

    добавил слайдер и блок популярных товаров

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,15 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+	const swiper = new Swiper(".mySwiper", {
+		spaceBetween: 30,
+		navigation: {
+		  nextEl: ".swiper-button-next",
+		  prevEl: ".swiper-button-prev",
+		},
+		pagination: {
+		  el: ".swiper-pagination",
+		},
+	  });
 });
