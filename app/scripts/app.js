2f7fc2ebf2e0ed6fd01c6aba3837463c008d24af
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-02-21 11:43:17 +0300

    табы и слайдер

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import { active } from 'browser-sync';
 
 $(() => {
 	svg4everybody();
@@ -14,4 +15,28 @@ $(() => {
 		  el: ".swiper-pagination",
 		},
 	  });
+
+	const slide = new Swiper(".Swiper", {
+		spaceBetween: 30,
+		pagination: {
+		  el: ".swiper-pagination",
+		},
+	  });
+
+	  const swiperproducts = new Swiper(".productsSwiper", {
+		spaceBetween: 30,
+		navigation: {
+		  nextEl: ".sale__button__next",
+		  prevEl: ".sale__button__prev",
+		},
+		});
+
+	
+	$(function() {
+		$(".sale__wrapper-content").not(":first").hide()
+		$(".sale__list-item").on("click", function() {
+			$(".sale__list-item").removeClass("active").eq($(this).index()).addClass("active")
+
+		})
+	});
 });
